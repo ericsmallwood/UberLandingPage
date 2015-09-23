9a4d345d3fe25b1e3afddaf5633ff8c121b132b4
unknown <ESmallwood@SW221749.ellis.com>
2015-09-23 15:56:22 -0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ESmallwood on 9/23/2015.
+ */
+$(document).ready(function(){
+    $('#imageSlider').sliderPro({
+        width:'100%',
+        height: 700,
+        fade: true,
+        buttons: false,
+        arrows: true
+    });
+});
+
